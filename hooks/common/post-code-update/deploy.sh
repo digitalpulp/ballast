c5aeef664253490627d2c5ebad0c1a6af72e69c8
Shawn Duncan <sduncan@digitalpulp.com>
2018-03-14 11:00:17 -0400

    Issue #9: Refactor cloud hooks to work with Drush 9 on acquia.

--- hooks/common/post-code-update/deploy.sh
+++ hooks/common/post-code-update/deploy.sh
@@ -7,18 +7,19 @@
 site="$1"
 target_env="$2"
 
+# Change working directory.
+cd /var/www/html/${site}.${target_env}/docroot/sites/default
 # Maintenance on.
-/var/www/html/${site}.${target_env}/vendor/bin/drush @$site.$target_env sset system.maintenance_mode 1 -y
-
+/var/www/html/${site}.${target_env}/vendor/bin/drush sset system.maintenance_mode 1 -y
 
 # Update database.
-/var/www/html/${site}.${target_env}/vendor/bin/drush @$site.$target_env updb -y
+/var/www/html/${site}.${target_env}/vendor/bin/drush updb -y
 
 # Config Import
-/var/www/html/${site}.${target_env}/vendor/bin/drush @$site.$target_env cim sync -y
+/var/www/html/${site}.${target_env}/vendor/bin/drush cim sync -y
 
 # Clear cache
-/var/www/html/${site}.${target_env}/vendor/bin/drush @$site.$target_env cr
+/var/www/html/${site}.${target_env}/vendor/bin/drush cr
 
 # Maintenance off.
-/var/www/html/${site}.${target_env}/vendor/bin/drush @$site.$target_env sset system.maintenance_mode 0 -y
+/var/www/html/${site}.${target_env}/vendor/bin/drush sset system.maintenance_mode 0 -y
